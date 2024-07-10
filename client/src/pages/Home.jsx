d18cefcb40d4e9bf8a64eda5e7844d5e590892f4
ravinderh17 <ravinderh17@gmail.com>
2024-07-10 09:24:09 +0530

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -71,7 +71,7 @@ export default function Home() {
     </div>
   
   </div> 
-  <img src="../../dist/bg.jpg" alt="" className='w-[95vh] h-[75vh] flex float-right pr-0 mr-0 md:mx-auto hidden lg:block' />
+  <img src="../../public/bg.jpg" alt="" className='w-[95vh] h-[75vh] flex float-right pr-0 mr-0 md:mx-auto hidden lg:block' />
  
 
 </div>
